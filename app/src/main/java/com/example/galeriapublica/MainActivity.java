4ea3906d8106803ef07a3993d631ce4d618bf3a3
20221tiimi0033 <marialiramoreira7@gmail.com>
2024-10-11 09:03:02 -0300

    layout 1

--- app/src/main/java/com/example/galeriapublica/MainActivity.java
+++ app/src/main/java/com/example/galeriapublica/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.galeriapublica;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +28,42 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        void setFragment(Fragment fragment) {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragContainer, fragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
+        @Override
+        protected void onResume() {
+            super.onResume();
+            List<String> permissions = new ArrayList<>();
+            permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+            checkForPermissions(permissions);
+        }
+        BottomNavigationView bottomNavigationView;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            final MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
+            bottomNavigationView = findViewById(R.id.btNav);
+            bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    vm.setNavigationOpSelected(item.getItemId());
+                    switch (item.getItemId()) {
+                        case R.id.gridViewOp:
+                            GridViewFragment gridViewFragment = GridViewFragment.newInstance();
+                            setFragment(gridViewFragment);
+                            break;
+                        case R.id.listViewOp:
+                            ListViewFragment listViewFragment = ListViewFragment.newInstance();
+                            setFragment(listViewFragment);
+                            break;
+                    }
+                    return true;
+                }
+        });
     }
-}
---
+++ app/src/main/java/com/example/galeriapublica/MainViewModel.java
@@ -0,0 +1,22 @@
+package com.example.galeriapublica;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class MainViewModel extends AndroidViewModel {
+
+    int navigationOpSelect R.id.gridViewOp;
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+    }
+    public int getNavigationOpSelected() {
+        return navigationOpSelected;
+    }
+    public void setNavigationOpSelected(int navigationOpSelected) {
+        this.navigationOpSelected = navigationOpSelected;
+    }
+
+}
