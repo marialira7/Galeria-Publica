7c297f7579a28feb913b2fb3a154bcb5df448140
20221tiimi0033 <marialiramoreira7@gmail.com>
2024-10-15 11:01:15 -0300

    layout 1

--- app/src/main/java/com/example/galeriapublica/MainActivity.java
+++ app/src/main/java/com/example/galeriapublica/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.ViewModelProvider;
@@ -17,6 +18,9 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //definindo o bottonViewNavigation como atributo para utilizarmos de refências para outros métodos
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,31 +32,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        void setFragment(Fragment fragment) {
-            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.fragContainer, fragment);
-            fragmentTransaction.addToBackStack(null);
-            fragmentTransaction.commit();
-        }
+        //configurando as ações que serão executadas assim que o usuário usar algum dos itens da nav dos botões
+
         @Override
-        protected void onResume() {
-            super.onResume();
-            List<String> permissions = new ArrayList<>();
-            permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
-            checkForPermissions(permissions);
-        }
-        BottomNavigationView bottomNavigationView;
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
+        protected void onCreate (Bundle savedInstanceState){
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
 
+            //obtendo referências
             final MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
+
+            //setando a função de "escutador" para selecionar o item que foi clicado
             bottomNavigationView = findViewById(R.id.btNav);
             bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    //guardando o item que foi escolhido anteriormente dentro do MainViewModel
                     vm.setNavigationOpSelected(item.getItemId());
+
+                    //definindo as ações que serão realizadas para cada opção
                     switch (item.getItemId()) {
                         case R.id.gridViewOp:
                             GridViewFragment gridViewFragment = GridViewFragment.newInstance();
@@ -63,7 +62,14 @@ public class MainActivity extends AppCompatActivity {
                             setFragment(listViewFragment);
                             break;
                     }
+                    }
                     return true;
                 }
-        });
+            });
+        }
+    void setFragment (Fragment fragment)
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragContainer, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
